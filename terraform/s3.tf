327bd74c04ed98b0ff76826d88c9d977398d2ebe
charltonmc2024 <charltonmc2022@gmail.com>
2026-07-31 00:05:41 -0500

    My Latest Codes

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,4 @@
 resource "aws_s3_bucket" "artifacts" {
-  bucket = "${var.app_name}-artifacts-1980"
+  bucket        = "${var.app_name}-artifacts-1980"
+  force_destroy = true
 }
--- terraform/terraform/s3.tf
+++ terraform/terraform/s3.tf
@@ -3,7 +3,8 @@ resource "random_id" "artifact_suffix" {
 }
 
 resource "aws_s3_bucket" "artifacts" {
-  bucket = "aws-terraform-cicd-artifacts-${random_id.artifact_suffix.hex}"
+  bucket        = "aws-terraform-cicd-artifacts-${random_id.artifact_suffix.hex}"
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "artifacts" {
